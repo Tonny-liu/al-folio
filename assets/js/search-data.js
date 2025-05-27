6ddb08d9a4b7f5fc99dbada6b54882d38ff5cf61
Tonny-liu <tengliude@gmail.com>
2025-05-27 06:31:39 +0000

    Deploying to gh-pages from @ Tonny-liu/al-folio@fff16d26a78d3cbf9bfa97c59ad50d0ff29ca772 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -399,8 +399,8 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/al-folio/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-new-preprint-related-to-resilience-measurement-sparkles",
+          title: 'New preprint related to resilience measurement! :sparkles:',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
