c386bee2ee3771cdf9606d2a1982c9eefb77d470
Tonnyliu <tengliude@gmail.com>
2026-07-30 13:08:59 +0200

    07-30

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,19 +1,61 @@
 $(document).ready(function () {
+  const $pubEntry = (el) => $(el).closest(".row");
+
+  const updatePubDropdownActiveState = ($entry) => {
+    $entry.find(".pub-links-dropdown .dropdown-item").each(function () {
+      const $item = $(this);
+      let sectionSelector = null;
+      if ($item.hasClass("abstract")) sectionSelector = ".abstract.hidden";
+      else if ($item.hasClass("award")) sectionSelector = ".award.hidden";
+      else if ($item.hasClass("bibtex")) sectionSelector = ".bibtex.hidden";
+
+      if (sectionSelector && $entry.find(sectionSelector).first().hasClass("open")) {
+        $item.addClass("active");
+      } else {
+        $item.removeClass("active");
+      }
+    });
+  };
+
+  const openPubSection = ($entry, sectionClass) => {
+    $entry.find(".abstract.hidden, .award.hidden, .bibtex.hidden").removeClass("open");
+    $entry.find(`.${sectionClass}.hidden`).addClass("open");
+    updatePubDropdownActiveState($entry);
+  };
+
+  const collapsePubSections = ($entry) => {
+    $entry.find(".abstract.hidden, .award.hidden, .bibtex.hidden").removeClass("open");
+    updatePubDropdownActiveState($entry);
+  };
+
+  // Collapse expanded sections when clicking the More button again
+  $(".pub-more-btn").click(function (e) {
+    const $entry = $pubEntry(this);
+    const hasOpen = $entry.find(".abstract.hidden.open, .award.hidden.open, .bibtex.hidden.open").length > 0;
+    if (hasOpen) {
+      e.preventDefault();
+      e.stopPropagation();
+      e.stopImmediatePropagation();
+      collapsePubSections($entry);
+      const $dropdown = $(this).closest(".dropdown");
+      $dropdown.removeClass("show");
+      $(this).attr("aria-expanded", "false");
+      $dropdown.find(".dropdown-menu").removeClass("show");
+    }
+  });
+
   // add toggle functionality to abstract, award and bibtex buttons
-  $("a.abstract").click(function () {
-    $(this).parent().parent().find(".abstract.hidden").toggleClass("open");
-    $(this).parent().parent().find(".award.hidden.open").toggleClass("open");
-    $(this).parent().parent().find(".bibtex.hidden.open").toggleClass("open");
+  $("a.abstract").click(function (e) {
+    e.preventDefault();
+    openPubSection($pubEntry(this), "abstract");
   });
-  $("a.award").click(function () {
-    $(this).parent().parent().find(".abstract.hidden.open").toggleClass("open");
-    $(this).parent().parent().find(".award.hidden").toggleClass("open");
-    $(this).parent().parent().find(".bibtex.hidden.open").toggleClass("open");
+  $("a.award").click(function (e) {
+    e.preventDefault();
+    openPubSection($pubEntry(this), "award");
   });
-  $("a.bibtex").click(function () {
-    $(this).parent().parent().find(".abstract.hidden.open").toggleClass("open");
-    $(this).parent().parent().find(".award.hidden.open").toggleClass("open");
-    $(this).parent().parent().find(".bibtex.hidden").toggleClass("open");
+  $("a.bibtex").click(function (e) {
+    e.preventDefault();
+    openPubSection($pubEntry(this), "bibtex");
   });
   $("a").removeClass("waves-effect waves-light");
 
